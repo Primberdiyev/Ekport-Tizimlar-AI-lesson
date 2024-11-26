d8b0742927641bac4fd67ef381705ad1a7c7ffba
Primberdiyev <158053951+Primberdiyev@users.noreply.github.com>
2024-11-26 22:57:52 +0500

    Add files via upload

---
+++ export_tizimlar.py
@@ -0,0 +1,71 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([35, 25, 25, 15])
+# myLabels = ["Sun'iy intellekt", 'Kiberxavfsizlik', 'Elektronika va sxemalar', 'Kompyuterni tashkil etish']
+
+# plt.pie(x, labels=myLabels, autopct='%1.1f%%')  
+# plt.title("Fan yo'nalishlari bo'yicha ulushlar")
+# plt.legend()
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linspace(0, 10, 100)
+# y = np.sin(x)
+
+# plt.plot(x, y, label='Sinus to‘lqini', color='blue')
+# plt.title('Sinus to‘lqini misoli')
+# plt.xlabel('X o‘qi')
+# plt.ylabel('Y o‘qi')
+# plt.legend()
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linspace(0, 10, 100)
+# y1 = np.sin(x)
+# y2 = np.cos(x)
+
+# plt.plot(x, y1, label='Sinus', color='red')
+# plt.plot(x, y2, label='Kosinus', color='green')
+# plt.title('Bir nechta grafik misoli')
+# plt.xlabel('X o‘qi')
+# plt.ylabel('Y o‘qi')
+# plt.legend()
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.random.rand(100)
+# y = np.random.rand(100)
+
+# plt.scatter(x, y, color='orange', edgecolor='black')
+# plt.title('Nuqtali diagramma misoli')
+# plt.xlabel('X o‘qi')
+# plt.ylabel('Y o‘qi')
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# kategoriyalar = ['A', 'B', 'C', 'D']
+# qiymatlar = [10, 20, 15, 25]
+
+# plt.bar(kategoriyalar, qiymatlar, color='cyan', edgecolor='black')
+# plt.title('Bar grafik misoli')
+# plt.xlabel('Kategoriyalar')
+# plt.ylabel('Qiymatlar')
+# plt.show()
